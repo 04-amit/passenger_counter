25f803aa11de53cee9c0fafa6d65c956245fe534
Amit Singh Chauhan <amit.skidmark.007@gmail.com>
2022-06-26 12:58:56 +0530

    prettier formatted code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let saveEl = document.getElementById("sa")
-let countEl = document.getElementById("count")
+let saveEl = document.getElementById("sa");
+let countEl = document.getElementById("count");
 
 let count = 0;
-function increment() {
-    count++;
-    countEl.innerText = count
-}
+const increment = () => {
+  count++;
+  countEl.innerText = count;
+};
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    count = 0
-    countEl.innerText = count
-}
+const save = () => {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  count = 0;
+  countEl.innerText = count;
+};
